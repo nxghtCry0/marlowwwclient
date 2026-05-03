0fae207631880463ea449d342144900e9d773cf7
nxghtCry0 <tejascerts@gmail.com>
2026-05-03 09:02:33 -0600

    feat: initialize project structure with fabric.mod.json, update versioning, and remove test artifacts

--- CameraTest.java
+++
@@ -1,7 +0,0 @@
-import net.minecraft.client.Minecraft;
-public class CameraTest {
-    public static void test() {
-        System.out.println(Minecraft.getInstance().getFrameTime());
-        System.out.println(Minecraft.getInstance().getTimer());
-    }
-}
